f9e1ee8270a8994e6e0e408ea405da2a02ac6b71
haru-13 <daikyou.hirasho.5002@gmail.com>
2023-05-31 19:40:48 +0900

    change config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,35 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images:{
+//     domains:["images.microcms-assets.io"],
+//   },
+// }
+
+// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{
-    domains:["images.microcms-assets.io"],
+  images: {
+    domains: ["images.microcms-assets.io"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
+};
